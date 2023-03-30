848534912820f8e5fe00e332f5586e2a58d1990b
Jakson Hensley <jaksonhensley@gmail.com>
2023-03-30 10:12:55 -0400

    Added Country Class and raw resource CSV's

---
+++ app/src/main/java/Country.java
@@ -0,0 +1,24 @@
+public class Country {
+
+    private String country;
+    private String continent;
+
+    public Country(String country, String continent) {
+
+        this.country = country;
+        this.continent = continent;
+
+    } // Country Constructor
+
+    public String getCountry() {
+
+        return this.country;
+
+    } // getCountry
+
+    public String getContinent() {
+
+        return this.continent;
+
+    } // getContinent
+} // Country
